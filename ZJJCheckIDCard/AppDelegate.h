0187a0fa3197cd5ced22b165cbf433d00d49010a
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-08-14 20:12:46 +0800

    Initial Commit

---
+++ ZJJCheckIDCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJCheckIDCard
+//
+//  Created by 张锦江 on 2017/8/14.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJJCheckIDCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJJCheckIDCard
+//
+//  Created by 张锦江 on 2017/8/14.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
